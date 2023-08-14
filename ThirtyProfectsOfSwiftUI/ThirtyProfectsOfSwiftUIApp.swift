ad5b2f75eee7296db8fbb73291755514c6641009
codelab21 <codelab21@outlook.com>
2023-08-14 13:13:46 +0300

    Initial Commit

---
+++ ThirtyProfectsOfSwiftUI/ThirtyProfectsOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirtyProfectsOfSwiftUIApp.swift
+//  ThirtyProfectsOfSwiftUI
+//
+//  Created by Eymen on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThirtyProfectsOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
